31cc5f58f044a3d7fbbd80c10f4d9358698e19bd
Paul Lupsa <paul.lupsa@ebs.msft>
2020-06-25 11:39:40 +0300

    Tema Sesiunea 2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('Hello World')
+### Lupsa Paul Tema Sesiune 2 ###
+
+### 1. Reverse the order of the items in an array ###
+
+a=[1,2,3,4,5]
+a.reverse()
+print('The items reversed of array a are :',a)
+
+
+### 2.Get the number of occurrences of var b in array a ###
+
+a = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b = 2
+print('The number of occurrences of var b in array a is :',a.count(b))
+
+### 3. Given a sentence as string, count the number of words in it ###
+
+a = 'ana are mere si nu are pere'
+print('The number of words in string a is :',len(a.split()))
